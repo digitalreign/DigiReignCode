cfda3958770c60952e0c43c2f7979b814bf4e393
digitalreign <muchenik@gmail.com>
2021-01-07 16:22:50 -0500

    Added Code training to the 7.1 exercise

--- CNN_with_TensorFlow_in_Python/Section71_CNN_with_TF_in_Python_exercise.py
+++ CNN_with_TensorFlow_in_Python/Section71_CNN_with_TF_in_Python_exercise.py
@@ -114,6 +114,7 @@ log_dir = "logs\\fit\\" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 # Train the network
+print('=============Train the network==============================================')
 model.fit(
     train_data, 
     epochs = NUM_EPOCHS, 
