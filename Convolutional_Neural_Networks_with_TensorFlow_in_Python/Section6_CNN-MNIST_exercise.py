4538a1f9555662be8b76936f8accee8cd8e30ba5
digitalreign <muchenik@gmail.com>
2021-01-06 19:19:10 -0500

    Tested the CNN model

--- Convolutional_Neural_Networks_with_TensorFlow_in_Python/Section6_CNN-MNIST_exercise.py
+++ Convolutional_Neural_Networks_with_TensorFlow_in_Python/Section6_CNN-MNIST_exercise.py
@@ -165,7 +165,44 @@ model.fit(
     verbose = 2
 )
 
+""" 422/422 - 6s - loss: 0.2780 - accuracy: 0.9202 - val_loss: 0.0891 - val_accuracy: 0.9738
+Epoch 2/20
+422/422 - 3s - loss: 0.0770 - accuracy: 0.9769 - val_loss: 0.0551 - val_accuracy: 0.9847
+Epoch 3/20
+422/422 - 3s - loss: 0.0549 - accuracy: 0.9828 - val_loss: 0.0547 - val_accuracy: 0.9852
+Epoch 4/20
+422/422 - 3s - loss: 0.0450 - accuracy: 0.9861 - val_loss: 0.0392 - val_accuracy: 0.9880
+Epoch 5/20
+422/422 - 2s - loss: 0.0374 - accuracy: 0.9882 - val_loss: 0.0262 - val_accuracy: 0.9917
+Epoch 6/20
+422/422 - 3s - loss: 0.0328 - accuracy: 0.9897 - val_loss: 0.0299 - val_accuracy: 0.9902
+Epoch 7/20
+422/422 - 3s - loss: 0.0285 - accuracy: 0.9912 - val_loss: 0.0242 - val_accuracy: 0.9925
+Epoch 8/20
+422/422 - 2s - loss: 0.0261 - accuracy: 0.9920 - val_loss: 0.0212 - val_accuracy: 0.9932
+Epoch 9/20
+422/422 - 2s - loss: 0.0221 - accuracy: 0.9927 - val_loss: 0.0166 - val_accuracy: 0.9952
+Epoch 10/20
+422/422 - 2s - loss: 0.0192 - accuracy: 0.9940 - val_loss: 0.0169 - val_accuracy: 0.9943
+Epoch 11/20
+422/422 - 3s - loss: 0.0176 - accuracy: 0.9945 - val_loss: 0.0188 - val_accuracy: 0.9960 """
 
+## Testing our model
+print('============================================================================')
+print('=============Testing our model==============================================')
+print('============================================================================')
+
+# Testing our model
+print('=============Testing our model==============================================')
+test_loss, test_accuracy = model.evaluate(test_data)
+
+"""1/1 [==============================] - 1s 622ms/step - loss: 0.0310 - accuracy: 0.9911"""
+
+# Printing the test results
+print('=============Printing the test results======================================')
+print('Test loss: {0:.4f}. Test accuracy: {1:.2f}%'.format(test_loss, test_accuracy*100.))
+
+""" Test loss: 0.0310. Test accuracy: 99.11% """
 
 # programend stores current time 
 programend = datetime.datetime.now()
